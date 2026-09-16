523335e222b11d4cfc88772034e20ca842e57b19
adnanshowkath005-tech <adnanshowkath005@gmail.com>
2026-09-16 12:48:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 import streamlit as st
-from generator import generate_itinerary
+from generator import generate_itinerary; from pdf_generator import create_itinerary_pdf
 from schemas import ItineraryRequest, BudgetLevel, Interest
-from pdf_generator import create_itinerary_pdf
+
 
 # Page Config
 st.set_page_config(
