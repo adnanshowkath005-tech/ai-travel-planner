6f3eb234cc727e26134381e74c79dc1ed4db9e2a
adnanshowkath005-tech <adnanshowkath005@gmail.com>
2026-09-16 12:52:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 import streamlit as st
-from generator import generate_itinerary; from pdf_generator import create_itinerary_pdf
+from generator import generate_itinerary
 from schemas import ItineraryRequest, BudgetLevel, Interest
-
-
+from pdf_generator import create_itinerary_pdf
 # Page Config
 st.set_page_config(
     page_title="AI Travel Planner",
